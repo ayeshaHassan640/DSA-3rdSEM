803387e20077b4ed3d38d09ed4c25b0b35f279f8
Ayesha Hassan Ansari <ansariayisha097@gmail.com>
2026-07-14 21:16:41 +0500

    Add files via upload

--- StackArray.cpp
+++ StackArray.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 #define SIZE 5 	//preprocessor directive ( before program runs,Compiler SIZE ko 5 se replace kar deta hai)
 int stack[SIZE];
-int top=-1;
+int top=-1;		//top=last element ka index
 //PUSH
 void push(int value){
 	if(top==SIZE-1){
@@ -21,10 +21,33 @@ void pop(){
 	cout << "Value Popped  " <<stack[top] << endl;
 	top--;
 }
+void peek()
+{
+	if(top!=-1)
+	{
+		cout <<"Top: " << stack[top]<< endl;
+	}
+}
 void display(){
 	for(int i=top;i>=0;i--){
 		cout <<stack[i];
 	}
+}
+//DisplayUsingRecursion
+void DisForRecur(int top)
+{
+	if(top<0)
+	return;
+	cout<< stack[top];
+	DisForRecur(top-1);
+}
+//DisplayUsingRecursion
+void DisBackRecur(int top)
+{
+	if(top<0)
+	return;
+	DisForRecur(top-1);
+	cout<< stack[top];
 }
 int main() {
     push(10);
@@ -32,12 +55,12 @@ int main() {
     push(30);
     pop();
     //display();
+    peek();
+    DisForRecur(top);
+    DisBackRecur(top);
 
     return 0;
 }
-
-
-
 
 
 
