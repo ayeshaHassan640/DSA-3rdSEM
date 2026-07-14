d1a225632c3f64ba2e60fd6853b5e3313b709f3c
Ayesha Hassan Ansari <ansariayisha097@gmail.com>
2026-07-14 21:16:17 +0500

    Add files via upload

--- Stack.cpp
+++ Stack.cpp
@@ -10,7 +10,6 @@ void push(int value){
 	Node* newNode=new Node;
 	newNode->data=value;
 	newNode->next=NULL;
-	Node* temp=top;
 	if(top==NULL){
 		top=newNode;
 		return;
@@ -29,10 +28,18 @@ void pop(){
 	top=top->next;
 	delete temp;
 }
+//Peek
+void peek()
+{
+	if(top!=NULL)
+	{
+		cout<< "Top: "<< top->data << endl;
+	}
+}
 //Display
-void display(){
+void displayFor(){
 	if(top==NULL){
-		cout<< "Stack Underflow";
+		cout<< "Stack Underflow"<< endl;
 		return;
 	}
 	Node* temp=top;
@@ -40,13 +47,30 @@ void display(){
 		cout<< temp->data<<endl;
 		temp=temp->next;
 	}
+}
 
+//DisplayUsing Recursion
+void displayForRecur(Node* top){
+	if(top==NULL){
+		return;
+	}
+	cout<<top->data<< "  ";
+	displayForRecur(top->next);
+}
+void displayBackRecur(Node* top){
+	if(top==NULL){
+		return;
+	}
+	displayBackRecur(top->next);
+	cout<<top->data<< "  ";
 }
 int main(){
 	push(22);
 	push(55);
 	push(35);
 	pop();
-	display();
+//	peek();
+	displayForRecur(top);
+	displayBackRecur(top);
 }
 
